31f080f717a0165195951185e0a8fcbb89d6f22a
Brice Videau <brice.videau@imag.fr>
2017-11-17 12:21:56 +0100

    Added single test.

---
+++ test/test.rb
@@ -0,0 +1,6 @@
+require 'set'
+require 'minitest/autorun'
+require_relative '../lib/libtree'
+require_relative 'test_libtree'
+require_relative 'test_automaton'
+require_relative 'test_homomorphism'
