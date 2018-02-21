d73172d3ddde02fe5b6f17dddad01b1f2e311228
Brice Videau <brice.videau@imag.fr>
2018-02-21 14:23:17 +0100

    Added support for ruby-graphviz.

---
+++ lib/libtree/graphviz.rb
@@ -0,0 +1,22 @@
+require 'graphviz'
+
+module LibTree
+  class Term
+
+    def to_graphviz( g = GraphViz::new( :G, :type => "strict digraph" ) )
+      to_graphviz_node(g)
+      g
+    end
+
+    def to_graphviz_node( g )
+      n =  g.add_nodes( self.object_id.to_s )
+      n[:label] = @symbol
+      cs = children.collect { |c| c.kind_of?(Term) ? c.to_graphviz_node(g) : g.add_nodes( c.to_s ) }
+      cs.each { |cn|
+        g.add_edges(n, cn)
+      }
+      n
+    end
+
+  end
+end
